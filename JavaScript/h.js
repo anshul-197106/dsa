3f038427b4ac1ff8ed4986305c08d326f9e148eb
anshul-197106 <kk9358324@gmail.com>
2026-02-09 14:03:57 +0530

    --

---
+++ JavaScript/h.js
@@ -0,0 +1,38 @@
+// JAVASCRIPT -- Dynamic Behaviour, Asynchronous,
+// single threading(one task at one time)
+// alert("External JS..")
+
+
+// var a = 20;           //functional scope, re-declare and re assign
+// let b = "Hello";      //block scope
+// const c = 30;         //block scope
+// console.log(a)
+
+// function hello(){
+    //     var x = 1;
+    //     if(x){
+        //         let a = 20;
+        //     }
+        //     console.log(a)
+        // }
+        // hello();
+ 
+
+// var a = 50;
+// var a = 40;
+// console.log(a)
+
+
+// let a = 20;          // global
+// function hello(){
+//     let a = 30;     // local
+//     console.log(a)
+// }
+// hello()
+
+
+// const a = 50;
+// a = 40;
+// console.log(a)
+
+
